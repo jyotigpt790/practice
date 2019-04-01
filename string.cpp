2b56d4150dabe11a15a38cfd0d0c46fc18ce694b
jyotigpt790 <48409873+jyotigpt790@users.noreply.github.com>
2019-04-01 13:02:31 +0530

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string first_name;
+	string Last_name;
+	string ful_name;
+	cout<<"enter your full name";
+	cin>>first_name>>Last_name;
+	if(first_name=="jyoti"||first_name=="JYOTI")
+		{
+		if(Last_name=="gupta"||Last_name=="GUPTA")
+		cout<<"your full name is jyoti gupta";
+		else
+		cout<<"last name miss match";
+		}
+	else
+	cout<<"name miss match";
+return 0;
+}
