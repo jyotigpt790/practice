21e2764d7969a34f7eaa460af493fc48a97d1e74
jyotigpt790 <48409873+jyotigpt790@users.noreply.github.com>
2019-04-24 13:53:02 +0530

    Add files via upload

---
+++ inline.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+inline void disp(int);
+int main()
+{
+	disp(150);
+	return 0;
+}
+inline void disp(int a)
+{
+	cout<<a<<" is output and it is an inline function";
+}
+
